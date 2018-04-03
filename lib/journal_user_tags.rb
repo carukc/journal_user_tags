7a84f84622fbbb175cc5e11634ae3ec39596c7b5
Florian Eck <florian.eck@el-digital.de>
2018-04-03 13:48:10 +0200

    bugfix

--- lib/journal_user_tags.rb
+++ lib/journal_user_tags.rb
@@ -2,9 +2,12 @@ module JournalUserTags
   
   def notified_users_with_easy_extensions
     tagged_user_names = self.notes.scan(/(?<=@)[A-Za-z0-9_\-\.]{1,}/).map(&:downcase)
-    tagged_users      = User.where("LOWER(login) IN (#{tagged_user_names.map {|u| "'#{u}'"}.join(",")})")
-    
-    tagged_users + super
+    if tagged_user_names.any?
+      tagged_users      = User.where("LOWER(login) IN (#{tagged_user_names.map {|u| "'#{u}'"}.join(",")})")
+      tagged_users + super
+    else
+      super
+    end  
   end
   
 end
