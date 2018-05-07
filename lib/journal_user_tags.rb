5810ab15184cf524fdc07b7650fc32ef3b07e854
Florian Eck <florian.eck@el-digital.de>
2018-05-07 13:18:06 +0200

    fixed nil exception when no notes given

--- lib/journal_user_tags.rb
+++ lib/journal_user_tags.rb
@@ -1,7 +1,7 @@
 module JournalUserTags
   
   def notified_users_with_easy_extensions
-    tagged_user_names = self.notes.scan(/(?<=@)[A-Za-z0-9_\-\.]{1,}/).map(&:downcase)
+    tagged_user_names = self.notes.to_s.scan(/(?<=@)[A-Za-z0-9_\-\.]{1,}/).map(&:downcase)
     if tagged_user_names.any?
       tagged_users      = User.where("LOWER(login) IN (#{tagged_user_names.map {|u| "'#{u}'"}.join(",")})")
       tagged_users + super
