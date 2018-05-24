2532f08f51fba74bd273ab53cc0e4c96497fc6c0
Florian Eck <florian.eck@el-digital.de>
2018-05-24 17:27:17 +0200

    added answer button

---
+++ config/routes.rb
@@ -0,0 +1,1 @@
+get "/issues/:id/answer/:to_user_id" => "issues#answer", as: :issues_answer
--- lib/journal_user_tags.rb
+++ lib/journal_user_tags.rb
@@ -1,15 +1,38 @@
 module JournalUserTags
   
-  def notified_users_with_easy_extensions
-    tagged_user_names = self.notes.to_s.scan(/(?<=@)[A-Za-z0-9_\-\.]{1,}/).map(&:downcase)
-    if tagged_user_names.any?
-      tagged_users      = User.where("LOWER(login) IN (#{tagged_user_names.map {|u| "'#{u}'"}.join(",")})")
-      tagged_users + super
-    else
-      super
-    end  
+  module JournalExtension
+  
+    def notified_users_with_easy_extensions
+      tagged_user_names = self.notes.to_s.scan(/(?<=@)[A-Za-z0-9_\-\.]{1,}/).map(&:downcase)
+      if tagged_user_names.any?
+        tagged_users      = User.where("LOWER(login) IN (#{tagged_user_names.map {|u| "'#{u}'"}.join(",")})")
+        tagged_users + super
+      else
+        super
+      end  
+    end
+  end  
+  
+  class Hooks < Redmine::Hook::ViewListener
+    render_on :view_issue_sidebar_top, :partial => 'issues/issue_answer_button'
   end
   
 end
 
-Journal.send :prepend, JournalUserTags
+IssuesController.class_eval do
+  
+  skip_before_action :authorize, only: [:answer]
+  
+  def answer
+    @issue = Issue.find(params[:id])
+    @project = @issue.project
+    edit
+
+    @issue.assigned_to_id = params[:to_user_id]
+
+    render "edit"
+  end
+end
+
+
+Journal.send :prepend, JournalUserTags::JournalExtension
